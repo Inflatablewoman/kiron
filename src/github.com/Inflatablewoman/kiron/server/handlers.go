5cf3bd0f1cbafc4ab1b97fdb0b13a8f22274a178
Keith Ball <keith.ball@brainloop.com>
2015-11-28 19:26:40 +0100

    added example of json struct tag

--- src/github.com/Inflatablewoman/kiron/server/handlers.go
+++ src/github.com/Inflatablewoman/kiron/server/handlers.go
@@ -63,22 +63,24 @@ func RegisterHTTPHandlers(mux *tigertonic.TrieServeMux) {
 	// Create comment
 	mux.Handle("POST", "/api/v1/users/{userID}/application/{applicationID}/comments", tigertonic.WithContext(tigertonic.If(getContext, tigertonic.Marshaled(createComment)), AuthContext{}))
 
-	// Create comment
-	mux.Handle("DELETE", "/api/v1/users/{userID}/application/{applicationID}/comments/{commentID}", tigertonic.WithContext(tigertonic.If(getContext, tigertonic.Marshaled(deleteComment)), AuthContext{}))
+}
 
+type createUserRequest struct {
+	EmailAddress string `json:"emailAddress"`
+	Name         string `json:"name"`
 }
 
 // createUser will create a user
-func createUser(u *url.URL, h http.Header, _ interface{}) (int, http.Header, *User, error) {
+func createUser(u *url.URL, h http.Header, request createUserRequest) (int, http.Header, *User, error) {
 	var err error
 	defer CatchPanic(&err, "createUser")
 
 	log.Println("createUser Started")
 
-	user := User{EmailAddress: "example@brainloop.com"}
+	user := User{EmailAddress: "example@brainloop.com", FirstName: request.Name}
 
 	// All good!
-	return http.StatusCreated, nil, &user, nil
+	return http.StatusOK, nil, &user, nil
 }
 
 // getUser will get a user
@@ -172,7 +174,7 @@ func createDocuments(u *url.URL, h http.Header, _ interface{}) (int, http.Header
 	//documents := nil
 
 	// All good!
-	return http.StatusCreated, nil, nil, nil
+	return http.StatusOK, nil, nil, nil
 }
 
 func getComments(u *url.URL, h http.Header, _ interface{}) (int, http.Header, []*Comment, error) {
@@ -193,19 +195,6 @@ func createComment(u *url.URL, h http.Header, _ interface{}) (int, http.Header, 
 	defer CatchPanic(&err, "createComment")
 
 	log.Println("createComment Started")
-
-	// TODO Implement functionality
-	//comment := nil
-
-	// All good!
-	return http.StatusOK, nil, nil, nil
-}
-
-func deleteComment(u *url.URL, h http.Header, _ interface{}) (int, http.Header, *Comment, error) {
-	var err error
-	defer CatchPanic(&err, "deleteComment")
-
-	log.Println("deleteComment Started")
 
 	// TODO Implement functionality
 	//comment := nil
