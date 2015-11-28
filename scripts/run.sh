29b415b440dafec0de30c6054c9cd030e1c53dc3
Marco Di Sarno <m.g.disarno@googlemail.com>
2015-11-28 21:46:53 +0100

    Merge

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 export KIRON_HOST=localhost
-export KIRON_PORT=80
+export KIRON_PORT=979
 
 export DBCONN="host=doxmon.cloudapp.net port=5432 dbname=kiron user=postgres password=Kironpassword1 sslmode=disable"
 
