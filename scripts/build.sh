3c3606796c51ff5c5a925fa8f293fb27bf01aaa3
Keith Ball <keith.ball@brainloop.com>
2015-11-28 21:18:46 +0100

    added script to start server

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,1 @@
-go get "github.com/rcrowley/go-tigertonic"
-go get "github.com/lib/pq"
+go install github.com/Inflatablewoman/kiron
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+export KIRON_HOST=localhost
+export KIRON_PORT=80
+
+export DBCONN="host=doxmon.cloudapp.net port=5432 dbname=kiron user=postgres password=Kironpassword1 sslmode=disable"
+
+kiron
