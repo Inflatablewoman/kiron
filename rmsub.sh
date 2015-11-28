66920b18d018d5088c98e192e26f22aa8f405ee6
Keith Ball <keith.ball@brainloop.com>
2015-11-28 19:54:40 +0100

    removed sub repo

--- rmsub.sh
+++ rmsub.sh
@@ -1,10 +1,2 @@
-rm -rf src/golang.org/x/tools/.git/ 
-git rm --cached src/golang.org/x/tools
-rm -rf src/github.com/golang/lint/.git/ 
-git rm --cached src/github.com/golang/lint
-rm -rf src/github.com/rogpeppe/godef/.git/ 
-git rm --cached src/github.com/rogpeppe/godef
-rm -rf src/github.com/nsf/gocode/.git/ 
-git rm --cached src/github.com/nsf/gocode
-rm -rf src/9fans.net/go/.git/ 
-git rm --cached src/9fans.net/go
+rm -rf src/golang.org/x/crypto/.git/ 
+git rm --cached src/golang.org/x/crypto
