b673f1d63e6caa44dcd3e1f323bbc968481ab318
Keith Ball <keith.ball@brainloop.com>
2015-11-28 19:20:49 +0100

    fix deps

--- rmsub.sh
+++ rmsub.sh
@@ -1,12 +1,10 @@
-rm -rf src/github.com/rcrowley/go-tigertonic/.git/ 
-git rm --cached src/github.com/rcrowley/go-tigertonic
-rm -rf src/github.com/rcrowley/go-metrics/.git/ 
-git rm --cached src/github.com/rcrowley/go-metrics
-rm -rf src/github.com/stretchr/testify/.git/ 
-git rm --cached src/github.com/stretchr/testify
-rm -rf src/github.com/pmezard/go-difflib/.git/ 
-git rm --cached src/github.com/pmezard/go-difflib
-rm -rf src/github.com/lib/pq/.git/ 
-git rm --cached src/github.com/lib/pq
-rm -rf src/github.com/davecgh/go-spew/.git/ 
-git rm --cached src/github.com/davecgh/go-spew
+rm -rf src/golang.org/x/tools/.git/ 
+git rm --cached src/golang.org/x/tools
+rm -rf src/github.com/golang/lint/.git/ 
+git rm --cached src/github.com/golang/lint
+rm -rf src/github.com/rogpeppe/godef/.git/ 
+git rm --cached src/github.com/rogpeppe/godef
+rm -rf src/github.com/nsf/gocode/.git/ 
+git rm --cached src/github.com/nsf/gocode
+rm -rf src/9fans.net/go/.git/ 
+git rm --cached src/9fans.net/go
